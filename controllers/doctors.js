d91c683895059ddeba25b363ea0fcf1e6264f033
Adem-abderrazek <Adem11.abderrazek@gmail.com>
2024-04-24 15:43:39 +0100

    separating the request from the users

--- controllers/doctors.js
+++ controllers/doctors.js
@@ -137,41 +137,9 @@ const createMedExp = async (req, res) => {
 };
 
 
-const getRequests = async (req, res) => {
-  try {
-    const { doctorId } = req.params;
-    // Fetch pending requests for the specified doctor
-    const pendingRequests = await prisma.request.findMany({
-      where: {
-        doctorId: parseInt(doctorId),
-        status: 'Pending'
-      }
-    });
-    res.status(200).json(pendingRequests);
-  } catch (error) {
-    console.error('Error fetching pending requests:', error);
-    res.status(500).json({ error: 'Internal server error' });
-  }
-}
 
-const handleReq = async (req, res) => {
-  try {
-    const { requestId } = req.params;
-    // Update the status of the request to 'Accepted'
-    const updatedRequest = await prisma.request.update({
-      where: {
-        id: parseInt(requestId)
-      },
-      data: {
-        status: 'Accepted'
-      }
-    });
-    res.status(201).json(updatedRequest);
-  } catch (error) {
-    console.error('Error updating request status:', error);
-    res.status(500).json({ error: 'Internal server error' });
-  }
-}
+
+
 
 
 
@@ -195,12 +163,10 @@ const getAllPatient = async (req, res) => {
 
 module.exports = {
   signup,
-  getOne,
   signin,
   getAllPatient,
   updatePatientMed,
   search,
-  handleReq,
   createMedExp,
-  getRequests,
+  
 };
--- routes/doctors.js
+++ routes/doctors.js
@@ -1,15 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { signup, getOne, signin, getAllPatient, updatePatientMed, search, handleReq, createMedExp,getRequests } = require('../controllers/doctors.js')
+const { signup, signin, getAllPatient, updatePatientMed, search, handleReq, createMedExp,getRequests } = require('../controllers/doctors.js')
 
 router.post("/signup", signup);
 router.post("/signin", signin);
 router.post("/createMedExp/:doctor_id", createMedExp)
 router.get("/getAllPatient", getAllPatient)
-router.get("/getOnePatient/:id", getOne)
-router.get('/requests/:doctorId',getRequests)
-router.put('/requests/:requestId',handleReq)
-
 router.put("/repport/:id", updatePatientMed)
 router.get("/search/:searched", search);
 
