0e748d7f2c0b80aaf40de501f6439729842b97db
Adem-abderrazek <Adem11.abderrazek@gmail.com>
2024-04-25 00:50:15 +0100

    GETING PERSONAL AND MEDICAL INFO OF THE PATIENT FOR  THE DOCTOR

--- routes/doctors.js
+++ routes/doctors.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const router = express.Router();
 
-const { signup, signin, getAllPatient, createMedExp ,getPatientsToDoctor} = require('../controllers/doctors.js')
+const { signup, signin, getAllPatient, createMedExp ,getPatientsToDoctor,getOne} = require('../controllers/doctors.js')
 
 
 
 router.post("/signup", signup);
+
 router.post("/signin", signin);
+
 router.post("/createMedExp/:doctor_id", createMedExp)
+
 router.get("/getAllPatient", getAllPatient)
+
 router.get("/:doctorId/patients",getPatientsToDoctor )
+
+router.get("/patient/:patientId",getOne)
 
 module.exports = router    
