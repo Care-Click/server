ddc8adcf043746a40e873d3916ec4071987a02c7
Adem-abderrazek <Adem11.abderrazek@gmail.com>
2024-04-24 15:43:54 +0100

    setting req route

--- routes/requests.js
+++ routes/requests.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const router = express.Router();
-
+const {getRequests,
+    createReport,
+    accepteRequest
+  } = require("../controllers/requests");
+router.post("/createRepport/:request",createReport);
+router.get("/accepteRequest/:requestId/:token",accepteRequest);
+router.get('/requests/:token',getRequests)
 
 
 
