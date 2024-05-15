0e613a4151dfc37542d21dae1ebd185164f1b0b4
Adem-abderrazek <Adem11.abderrazek@gmail.com>
2024-05-15 12:19:16 +0100

    adding verificatiion emal

--- routes/doctors.js
+++ routes/doctors.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const isDoctorAuthenticated=require('../middlewares/isDoctorAuthenticated.js')
 
-const { signup, signin, createMedExp ,getDoctorPatients,getOnePatient,getDoctor,updateDoctors} = require('../controllers/doctors.js')
+const { signup, signin, createMedExp ,getDoctorPatients,getOnePatient,getDoctor,updateDoctors,verifydoctor} = require('../controllers/doctors.js')
 
 router.post("/signup", signup);
 
@@ -17,6 +17,8 @@ router.get("/patients", isDoctorAuthenticated, getDoctorPatients)
 router.get("/patient/:patientId", getOnePatient)
 
 router.put("/updateProfile",isDoctorAuthenticated,updateDoctors)
+
+router.post("/verify/:doctorId",verifydoctor)
 
 
 module.exports = router    
